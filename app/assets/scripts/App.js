07cb22ced360477a4745689d3a9f0cfbbc91361a
Hyrave <hyrave@gmail.com>
2020-10-19 10:56:39 +0800

    adjustment to header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,22 @@
 import '../styles/styles.css'
 
-
+let SlideImages = document.querySelectorAll(".header-logos ul li");
+var max = SlideImages.length;
+var i = 0;
+var list = [0,1,2,3,4];
+setInterval(function() {
+  if(i>4) { i=0;}  
+  SlideImages[i].classList.add("header-logos--is-visible");
+  console.log("i:"+i+" classlist:"+SlideImages[i].classList.length);  
+  for(var ii=0;ii<list.length;ii++) {    
+    if(i != list[ii]) {
+      console.log("removing class of index"+list[ii]+" and retain index"+i);
+      SlideImages[list[ii]].classList.remove("header-logos--is-visible");
+    }
+  }
+  i++;
+},5000);
+  
 
 if(module.hot) {
   module.hot.accept()
